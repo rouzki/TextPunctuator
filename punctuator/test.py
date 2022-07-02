26bc26dc1a9d18780046bfdd6bac2d82d1eafc5f
Zakarya ROUZKI <zakarya.rouzki.fycomputing@gmail.com>
2022-07-02 15:59:46 +0100

    update test.py

--- punctuator/test.py
+++ punctuator/test.py
@@ -5,10 +5,13 @@ __email__ = "zakaryarouzki@gmail.com"
 
 
 from punctuator import TextPonctuator
+import logging
 
 punctuator = TextPonctuator(use_gpu=False)
 
 
-text = 'this is a test text with some formatting rules that should not be displayed inputs and outputs'
+text = "Sur la base de ces échanges Blake Lemoine a donc jugé que le système avait atteint un niveau de conscience lui permettant d’être sensible Ce dernier a ensuite envoyé par email un rapport sur la sensibilité supposée de LaMDA à deux cents employés de Google Très vite les dirigeants de l’entreprise ont rejeté les allégations"
 
 text_punctuated = punctuator.punctuate(text, lang='fr')
+
+ logging.info(f"text ponctuated: [ {len(text_punctuated)} ].")
